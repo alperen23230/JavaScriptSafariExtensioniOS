1ff9e7da3b92ae3a7273f6f85758f890b29f9bb5
Alperen Ünal <alperenunal@Alperens-MacBook-Air.local>
2019-04-12 22:15:30 +0300

    Initial Commit

---
+++ JavascriptSafariExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JavascriptSafariExtension
+//
+//  Created by Alperen Ünal on 12.04.2019.
+//  Copyright © 2019 Alperen Ünal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
